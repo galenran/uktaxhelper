d73de03f0b3aaa4b74cff022678bd369173ce090
GalenRan <504350949@qq.com>
2026-02-06 11:58:21 +0800

    Configure sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     output: 'static',
-    site: 'https://example.com'
+    site: 'https://uktaxhelper.co.uk',
+    integrations: [sitemap()]
 });
